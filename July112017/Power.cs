474c3a19dc1a20ff4c6fb2ef1a3dd781539a4e62
300926123 <300926123@PR-A315-24>
2017-07-11 17:40:12 -0400

    craeted a power class

---
+++ July112017/Power.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace July112017
+{
+    /// <summary>
+    /// This is power class which will be used as a dtaa type
+    /// </summary>
+    class Power
+    {
+        //PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public  int RANK { get; set; }
+        //cONSTRUCTORS----------------------------------------------
+    }
+}
